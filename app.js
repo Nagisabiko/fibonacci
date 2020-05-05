cd113bf2fd9ee4df21bf2390c2c8937f9bbcd822
Nagisabiko <7ag1sagoo@gmail.com>
2020-05-05 15:15:11 +0900

    メモ化によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 'use strict';
-//Fib関数を作る
-//フィボナッチ数列の一番目と二番目はお決まりなので計算ではなくルールで値を返す
+const memo = new Map();
+memo.set(0, 0);
+memo.set(1, 1);
 function fib(n) {
-    if (n === 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
+    if (memo.has(n)) {
+        return memo.get(n);
     }
-    //それ以外の場合
-    return fib(n - 1) + fib(n - 2);
+    const value = fib(n - 1) + fib(n - 2);
+    memo.set(n, value);
+    return value;
 }
-
-//40番目まで出力する
 const length = 40;
-for (let i = 0; i <= length; i++) {
+for (let i = 0; i <= length; i++ ) {
     console.log(i + '番目：' + fib(i));
 }
 
-//テスト「０」console.log(fib(0));
-//テスト「１」console.log(fib(1));
-//テスト「１０」console.log(fib(10));
+
+//Fib関数を作る
+//フィボナッチ数列の一番目と二番目はお決まりなので計算ではなくルールで値を返す
+//function fib(n) {
+//    if (n === 0) {
+//        return 0;
+//    } else if (n === 1) {
+//        return 1;
+//    }
+//    //それ以外の場合
+//    return fib(n - 1) + fib(n - 2);
+//}
+
+//40番目まで出力する
+//const length = 40;
+//for (let i = 0; i <= length; i++) {
+//    console.log(i + '番目：' + fib(i));
+//}
